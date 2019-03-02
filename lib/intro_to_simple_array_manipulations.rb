a906ec76cb80e52d7d6c96c9b8ecfcaec7e0f613
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 23:51:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_push(array, phrase)
+  array.push(phrase)
+end
+
+def using_unshift(arr, phrase)
+  arr.unshift(phrase)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr, arr2)
+  arr.concat(arr2)
+end
+
+def 
